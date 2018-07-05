3bbd984a8a298fe93fcc7733d03603a3ca94d00d
JoseDavi <jose.davi.souza@ccc.ufcg.edu.br>
2018-07-05 11:43:16 -0300

    nada

--- src/com/projeto/Facade.java
+++ src/com/projeto/Facade.java
@@ -16,7 +16,7 @@ public class Facade {
 							  "acceptance_tests/use_case4_exception.txt",
 							  "acceptance_tests/use_case4.txt" };
 		
-		EasyAccept.main(args);;
+		EasyAccept.main(args);
 		
 	}
 }
