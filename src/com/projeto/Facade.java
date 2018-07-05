3208ce55a098ae96e4d76968a6b3dd9e287ed4a8
Rich Elton <richelton14@gmail.com>
2018-07-05 11:24:14 -0300

    Adicionando o Easy

--- src/com/projeto/Facade.java
+++ src/com/projeto/Facade.java
@@ -1,5 +1,21 @@
 package com.projeto;
 
+import easyaccept.EasyAccept;
+
 public class Facade {
 
+	public static void main(String[] args) {
+		
+		args = new String[] { "com.projeto.Facade",
+						      "acceptance_tests/use_case1_exception.txt",
+							  "acceptance_tests/use_case1.txt",	  
+							  "acceptance_tests/use_case2_exception.txt",
+							  "acceptance_tests/use_case2.txt",
+							  "acceptance_tests/use_case3_exception.txt",
+							  "acceptance_tests/use_case3.txt",
+							  "acceptance_tests/use_case4_exception.txt",
+							  "acceptance_tests/use_case4.txt" };
+		EasyAccept.main(args);
+		
+	}
 }
