1acfc600d47c5f543d17d3a4290c315cfa2d4250
Matheus D10S <matheus.lemos@ccc.ufcg.edu.br>
2018-07-10 09:16:51 -0300

    Adicionando comentario nas classes de ordenação.

--- src/com/projeto/ordenacao/OrdenaItensPorCategoria.java
+++ src/com/projeto/ordenacao/OrdenaItensPorCategoria.java
@@ -5,7 +5,15 @@ import java.util.Comparator;
 import com.projeto.entidades.Item;
 
 public class OrdenaItensPorCategoria implements Comparator<Item> {
-
+	
+	/**
+	 * Classe responsavel por ordenar dois objetos por meio de sua categoria.
+	 * 
+	 * @author  Igor Silveira
+	 * 			Jose Davi
+	 * 			Matheus Gusmao
+	 * 			Rich Ramalho
+	 */
 	@Override
 	public int compare(Item o1, Item o2) {
 		return o1.getCategoria().compareTo(o2.getCategoria());
--- src/com/projeto/ordenacao/OrdenaItensPorNome.java
+++ src/com/projeto/ordenacao/OrdenaItensPorNome.java
@@ -5,7 +5,15 @@ import java.util.Comparator;
 import com.projeto.entidades.Item;
 
 public class OrdenaItensPorNome implements Comparator<Item> {
-
+	
+	/** 
+	 * Classe responsavel por comparar dois objetos por meio do nome.
+	 * 
+	 * @author  Igor Silveira
+	 * 			Jose Davi
+	 * 			Matheus Gusmao
+	 * 			Rich Ramalho
+	 */
 	@Override
 	public int compare(Item o1, Item o2) {
 		return o1.getNome().compareTo(o2.getNome());
